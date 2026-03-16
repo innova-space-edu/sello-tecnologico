e737d150b6a11908c63ac25b4d9489c3667b1217
Innova Space <emorales@colprovidencia.cl>
2026-03-16 10:46:22 -0300

    Update proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -64,6 +64,6 @@ export async function proxy(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|login|registro|bloqueado).*)',
+    '/((?!_next/static|_next/image|favicon.ico|.*\.(?:svg|png|jpg|jpeg|gif|webp|ico|css|js|map|txt|woff|woff2|ttf)$|login|registro|bloqueado).*)',
   ],
 }
