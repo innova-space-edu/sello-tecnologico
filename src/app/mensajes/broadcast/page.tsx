0d08b80cbcde855657e0973fe69705a79d1e86fc
Innova Space <emorales@colprovidencia.cl>
2026-06-17 14:57:44 -0400

    Add basic broadcast page

--- src/app/mensajes/broadcast/page.tsx
+++ src/app/mensajes/broadcast/page.tsx
@@ -1,1 +1,7 @@
-export default function Page(){return null}
+'use client'
+
+import Sidebar from '@/components/Sidebar'
+
+export default function Page(){
+  return <div className="flex min-h-screen bg-gray-50"><Sidebar /><main className="lg:ml-64 flex-1 p-8">Enviar mensaje grupal</main></div>
+}
