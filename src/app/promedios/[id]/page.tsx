165d166d523ff971f9a7e0facd079bcf285d27c7
Innova Space <emorales@colprovidencia.cl>
2026-09-03 14:10:23 -0400

    feat: activar atajos de teclado en Promedios

--- src/app/promedios/[id]/page.tsx
+++ src/app/promedios/[id]/page.tsx
@@ -2,6 +2,7 @@ import { notFound, redirect } from 'next/navigation'
 import Sidebar from '@/components/Sidebar'
 import LightSpreadsheet from '@/components/promedios/LightSpreadsheet'
 import PromediosHistory from '@/components/promedios/PromediosHistory'
+import PromediosKeyboardNavigation from '@/components/promedios/PromediosKeyboardNavigation'
 import { createAdminSupabaseClient } from '@/lib/supabase-admin'
 import { getPromediosActor } from '@/lib/promedios-auth'
 
@@ -28,6 +29,7 @@ export default async function PromediosEditorPage({ params }: Props) {
     <div className="min-h-screen bg-slate-100 lg:flex">
       <Sidebar />
       <LightSpreadsheet workbookId={workbook.id} title={workbook.title} />
+      <PromediosKeyboardNavigation />
       <PromediosHistory workbookId={workbook.id} />
     </div>
   )
