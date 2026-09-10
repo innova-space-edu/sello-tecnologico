23a2c5cb9d0376eed471a8dbb6ab1bd3f82cf415
Innova Space <emorales@colprovidencia.cl>
2026-09-10 11:11:07 -0300

    feat(promedios): usar editor avanzado con ONLYOFFICE

--- src/app/promedios/[id]/page.tsx
+++ src/app/promedios/[id]/page.tsx
@@ -1,8 +1,7 @@
 import { notFound, redirect } from 'next/navigation'
 import Sidebar from '@/components/Sidebar'
-import LightSpreadsheet from '@/components/promedios/LightSpreadsheet'
+import OnlyOfficeSpreadsheet from '@/components/promedios/OnlyOfficeSpreadsheet'
 import PromediosHistory from '@/components/promedios/PromediosHistory'
-import PromediosKeyboardNavigation from '@/components/promedios/PromediosKeyboardNavigation'
 import { createAdminSupabaseClient } from '@/lib/supabase-admin'
 import { getPromediosActor } from '@/lib/promedios-auth'
 
@@ -28,8 +27,7 @@ export default async function PromediosEditorPage({ params }: Props) {
   return (
     <div className="min-h-screen bg-slate-100 lg:flex">
       <Sidebar />
-      <LightSpreadsheet workbookId={workbook.id} title={workbook.title} />
-      <PromediosKeyboardNavigation />
+      <OnlyOfficeSpreadsheet workbookId={workbook.id} title={workbook.title} />
       <PromediosHistory workbookId={workbook.id} />
     </div>
   )
